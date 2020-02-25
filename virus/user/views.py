60b98213ac8b236c282541c66369e40293c8220c
Sehyeon Kim <zokatn@gmail.com>
2020-02-25 22:19:16 +0900

    bootstrap 추가 및 메인 화면

--- virus/user/views.py
+++ virus/user/views.py
@@ -11,17 +11,19 @@ def register(request):
         return render(request, 'register.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
-        if not (username and password and re_password):
+        if not (username and useremail and password and re_password):
             res_data['error'] = '모든 사항을 입력하세요.'
         if password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             user = User(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
 
