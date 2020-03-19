ed6035b13ea03c8ad647207beba5a061c3da136e
Sehyeon Kim <zokatn@gmail.com>
2020-03-19 18:02:33 +0900

    update 및 session

--- virus/user/views.py
+++ virus/user/views.py
@@ -1,13 +1,41 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 
 # Create your views here.
 
 
+def home(request):
+    user_id = request.session.get('user')
+
+    if user_id:
+        user = User.objects.get(pk=user_id)
+        return HttpResponse(user.username)
+
+    return HttpResponse("Home")
+
+
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = ' 모든 값을 입력하세요.'
+        else:
+            user = User.objects.get(username=username)
+            if check_password(password, user.password):
+                # 비밀번호가 일치, 로그인 처리
+                request.session['user'] = user.id
+                return redirect('/')
+            else:
+                res_data['error'] = '비밀번호가 틀렸습니다.'
+
+        return render(request, 'login.html', res_data)
 
 
 def register(request):
@@ -21,7 +49,7 @@ def register(request):
 
         res_data = {}
         if not (username and useremail and password and re_password):
-            res_data['error'] = '모든 사항을 입력하세요.'
+            res_data['error'] = '모든 값을 입력하세요.'
         if password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
