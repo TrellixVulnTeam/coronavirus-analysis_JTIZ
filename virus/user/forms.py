dbc87f52567b47ba7d02bbecdb511d97888d8521
Sehyeon Kim <zokatn@gmail.com>
2020-04-06 16:17:43 +0900

    board기능 생성

--- virus/user/forms.py
+++ virus/user/forms.py
@@ -1,6 +1,28 @@
 from django import forms
+from .models import User
+from django.contrib.auth.hashers import check_password
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=32, label="사용자 아이디")
-    password = forms.CharField(widget=forms.PasswordInput, label="비밀번호")
+    username = forms.CharField(
+        error_messages={
+            'required': '아이디를 입력해주세요.'
+        },
+        max_length=32, label="사용자 아이디")
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label="비밀번호")
+
+    def clean(self):
+        clean_data = super().clean()
+        username = clean_data.get('username')
+        password = clean_data.get('password')
+
+        if username and password:
+            user = User.objects.get(username=username)
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.user_id = user.id
--- virus/user/views.py
+++ virus/user/views.py
@@ -25,7 +25,13 @@ def logout(request):
 
 
 def login(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
 
